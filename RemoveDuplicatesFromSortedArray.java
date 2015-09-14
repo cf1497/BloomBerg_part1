87d0e5b69df9bbd298d32a09901b8e522edfc6de
cf1497 <cf1497@nyu.edu>
2015-09-14 18:13:07 -0400

    LeetCode remove I and remove II

--- RemoveDuplicatesFromSortedArray.java
+++ RemoveDuplicatesFromSortedArray.java
@@ -3,15 +3,23 @@ package Algorithm_Part1;
 public class RemoveDuplicatesFromSortedArray {
 	
 	public static void main(String[] args){
-		int[] nums={1,1,2,2,3,4,5,5,6};
-		System.out.println(removeDuplicates(nums));
+		int[] nums={1,1,2,2,2,2,3,3,4,5,5,6};
+		System.out.println(removeDuplicatesI(nums));
+		
+		for(int n:nums){
+			System.out.print(n+ " "); 
+		}
+		
+		System.out.println();
+		
+		System.out.println(removeDuplicatesII(nums));
 		
 		for(int n:nums){
 			System.out.print(n+ " "); 
 		}
 	}
 	
-	public static int removeDuplicates(int[] nums){
+	public static int removeDuplicatesI(int[] nums){
 		if(nums.length==0 || nums.length==1){
 			return nums.length;
 		}
@@ -29,5 +37,33 @@ public class RemoveDuplicatesFromSortedArray {
 		}
 		
 		return start+1;
+	}
+	
+	public static int removeDuplicatesII(int[] nums){
+		if(nums.length<3){
+			return nums.length;
+		}
+		
+		int start = 0, p = 0;
+		
+		int count =1;
+		
+		while(p<nums.length){
+			if(nums[start]==nums[p]){
+				if(count==1){
+					start++;
+					nums[start]= nums[p];
+				}
+				p++;
+				count++;
+			}else{
+				start++;
+				nums[start] = nums[p];
+				p++;
+			}
+		}
+		
+		return start+1;
+		
 	}
 }
